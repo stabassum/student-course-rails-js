adc820ac87fcdf54623d21a963d16e5a0a7e138f
stabassum <sumu1993@gmail.com>
2020-10-15 20:57:54 -0400

    student.js updated

--- student-course-frontend/src/student.js
+++ student-course-frontend/src/student.js
@@ -1,3 +1,22 @@
 class Student  {
     
+    constructor(student) {
+        this.id = student.id
+        this.full_name = student.full_name
+        // this.course = student.course_id ----- to establish relationship between student and course
+        this.email = student.email
+        this.time_preference = student.time_preference
+        this.course = student.course_id
+
+        // Student.all.push(this)
+    }
+
+    renderStudent(){
+
+    }
+
+
 }
+
+// store all the students in an array
+Student.all = [];
